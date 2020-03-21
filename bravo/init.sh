86a0ce757ed8184d18c60cd948b9d65aea3aff9e
msau1607 <matthias.sauer@mail.schwarz>
2020-03-21 23:07:39 +0100

    bravo: final fix

--- bravo/init.sh
+++ bravo/init.sh
@@ -1,4 +1,9 @@
 ssh node01 -- mkdir /drupal-data /drupal-mysql-data
 kubectl create secret generic drupal-mysql-secret --from-literal=MYSQL_ROOT_PASSWORD=root_password --from-literal=MYSQL_DATABASE=drupal-database --from-literal=MYSQL_USER=root
 cat *yaml > bravo.yaml
+kubectl apply -f 01_drupal-pv.yaml
+kubectl apply -f 01_drupal-pvc.yaml
+sleep 1
+kubectl apply -f 02_drupal-mysql-pv.yaml
+kubectl apply -f 02_drupal-mysql-pvc.yaml
 kubectl apply -f bravo.yaml
