4045fd94312f91b728f1d227a41f28548191d52d
msau1607 <matthias.sauer@mail.schwarz>
2020-03-21 03:14:57 +0100

    add apply all yaml to init.sh

--- bravo/init.sh
+++ bravo/init.sh
@@ -1,3 +1,4 @@
 ssh node01 -- mkdir /drupal-data /drupal-mysql-data
 kubectl create secret generic drupal-mysql-secret --from-literal=MYSQL_ROOT_PASSWORD=root_password --from-literal=MYSQL_DATABASE=drupal-database --from-literal=MYSQL_USER=root
-
+cat *yaml > bravo.yaml
+kubectl apply -f bravo.yaml
